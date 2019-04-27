665b9c53f95ddc1a18c7cf9ea0bcde26536f573b
DJ Zaragoza <zaragoza0126@gmail.com>
2019-04-27 10:18:19 -0700

    first commit of DOM II project creating event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+//event listener = click
+const btn = document.getElementsByClassName('.btn');
+
+//event listener = mouseover
+const a = document.getElementsByClassName('.a');
+
+//event listener = select
+const destination = document.getElementsByClassName('destination');
+
+//event listener for paragraphs = keydown
+const p = document.getElementById('p');
+
+//event listener = blur
+const imgDestination = document.getElementsByTagName('imgDestination');
+
