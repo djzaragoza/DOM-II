4ee325d4cb214c3152b8c0ed30e2e470334b3817
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-01 11:34:29 -0700

    commented out my code for now until i figure out how to fix it

--- js/index.js
+++ js/index.js
@@ -1,61 +1,61 @@
 // Your code goes here
 
-const logNew = document.querySelector('.logo-heading');
+// const logNew = document.querySelector('.logo-heading');
 
-logNew.addEventListener(`click`, function (event) {
-    console.log(logNew);
-})
+// logNew.addEventListener(`click`, function (event) {
+//     console.log(logNew);
+// })
 
-const headerTwo = document.querySelector('h2');
-headerTwo.addEventListener('dblclick', function(event) {
-    event.target.style.color = 'yellow';
-})
+// const headerTwo = document.querySelector('h2');
+// headerTwo.addEventListener('dblclick', function(event) {
+//     event.target.style.color = 'yellow';
+// })
 
-const para = document.querySelector('p');
+// const para = document.querySelector('p');
 
-para.addEventListener('mouseover', function(event) {
-    event.target.style.color = 'red';
-})
+// para.addEventListener('mouseover', function(event) {
+//     event.target.style.color = 'red';
+// })
 
-const newNav = document.querySelector('nav');
+// const newNav = document.querySelector('nav');
 
-newNav.addEventListener('mouseover', function(event) {
-    event.target.style.color = 'yellow';
-})
+// newNav.addEventListener('mouseover', function(event) {
+//     event.target.style.color = 'yellow';
+// })
 
-const newImg = document.querySelector('img');
+// const newImg = document.querySelector('img');
 
-newImg.addEventListener('mouseover', function(event) {
-    alert('Click now and enter to win!');
-    event.preventDefault();
-})
+// newImg.addEventListener('mouseover', function(event) {
+//     alert('Click now and enter to win!');
+//     event.preventDefault();
+// })
 
-const foot = document.querySelector('footer');
+// const foot = document.querySelector('footer');
 
-foot.addEventListener('mouseover', function(evebt) {
-    event.target.style.backgroundcolor = 'yellow';
-})
+// foot.addEventListener('mouseover', function(evebt) {
+//     event.target.style.backgroundcolor = 'yellow';
+// })
 
-const button = document.querySelector('.btn');
+// const button = document.querySelector('.btn');
 
-button.addEventListener('mouseover', function(event) {
-    event.target.style.backgroundcolor = 'red';
-})
+// button.addEventListener('mouseover', function(event) {
+//     event.target.style.backgroundcolor = 'red';
+// })
 
-const bttn = document.getElementById('bttn');
+// const bttn = document.getElementById('bttn');
 
-bttn.addEventListener('mouseover', function(event) {
-    event.target.style.backgroundcolor = 'black';
-})
+// bttn.addEventListener('mouseover', function(event) {
+//     event.target.style.backgroundcolor = 'black';
+// })
 
-const headFour = document.querySelector('h4');
+// const headFour = document.querySelector('h4');
 
-headFour.addEventListener('mouseover', function(event) {
-    console.log('The event type: $(event.type)');
-})
+// headFour.addEventListener('mouseover', function(event) {
+//     console.log('The event type: $(event.type)');
+// })
 
-const headerNew = document.querySelector('.main-navigation');
+// const headerNew = document.querySelector('.main-navigation');
 
-headerNew.addEventListener('mouseleave', event => {
-    event.target.style.backgroundcolor = 'teal';
-})
+// headerNew.addEventListener('mouseleave', event => {
+//     event.target.style.backgroundcolor = 'teal';
+// })
