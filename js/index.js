d9ea03b51146676e4f24cd7e4e76f274ced31782
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-01 00:37:59 -0700

    still trying to figure out how to make proper event listeners

--- js/index.js
+++ js/index.js
@@ -1,42 +1,4 @@
 // Your code goes here
 
-//event listener = click
 
-const btn = document.getElementsByClassName(`.btn`);
-btn.addEventListener("click", (event) => {btn.style.background = "yellow"});
-console.log(btn);
-
-
-//event listener = mouseover
-const a = document.getElementsByClassName('.a');
-
-//event listener = select
-const destination = document.querySelectorAll('destination');
-destination.forEach(paragraph => {
-    paragraph.addEventListener('select', (event) => {
-        console.log("event", event);
-        paragraph.style.color = "magenta";
-    })
-})
-
-//event listener for paragraphs = keydown
-const p = document.getElementById('p');
-
-//event listener = blur
-const imgDestination = document.getElementsByTagName('imgDestination');
-
-//event listener = resize
-const funBus = document.getElementsByTagName('funBus');
-
-//event listener = double click
-const logoHeading = document.getElementsByClassName('logo-heading');
-
-//event listener = load
-const webPage = document.querySelectorAll('webPage');
-
-//event listener = abort
-const pageAbort = document.querySelectorAll('pageAbort');
-
-//event listener = focus
-const destinationImg = document.getElementsByClassName('destinationImg');
 
