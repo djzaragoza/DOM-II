47f41ec3714a271d3aeb721452c2a9419cb4c973
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-01 00:49:08 -0700

    added more event listeners

--- js/index.js
+++ js/index.js
@@ -1,4 +1,34 @@
 // Your code goes here
 
+const logNew = document.querySelector('.logo-heading');
+
+logNew.addEventListener(`click`, function(event) {
+    console.log(`The Bus is moving fast!`);
+})
+
+const headerTwo = document.querySelector('h2');
+headerTwo.addEventListener('dblclick', function(event) {
+    event.target.style.color = 'yellow';
+})
+
+const para = document.querySelector('p');
+
+para.addEventListener('mouseover', function(event) {
+    event.target.style.color = 'red';
+})
+
+const newNav = document.querySelector('nav');
+
+newNav.addEventListener('mouseover', function(event) {
+    event.target.style.color = 'yellow';
+})
+
+const newImg = document.querySelector('img');
+
+newImg.addEventListener('mouseover', function(event) {
+    alert('Click now and enter to win!');
+    event.preventDefault();
+})
+
 
 
